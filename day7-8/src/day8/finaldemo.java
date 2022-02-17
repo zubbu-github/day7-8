70d57f93c49c6cc69c12cdaf92c7e1fd9d419e4e
LENOVO <mdzubeir2468@gmail.com>
2022-02-17 12:10:55 +0530

    day 8 final demo

---
+++ day7-8/src/day8/finaldemo.java
@@ -0,0 +1,20 @@
+package day8;
+
+class A {
+	  int a = 20;
+	  final void m1() {
+		  a = 25;
+		  System.out.println("Value of a " + a);
+	  }
+	  void m2() {
+		  System.out.println("This is the method 2");
+	  }
+}
+public class finaldemo extends A{
+
+	public static void main(String[] args) {
+		A a =new A();
+		a.m1();
+		a.m2();
+	}
+}
